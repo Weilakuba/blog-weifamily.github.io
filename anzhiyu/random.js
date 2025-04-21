0e89e41a4a213eb3021e8ba31b9c611e0322670b
Oliver <oliver@weifamily.cn>
2025-04-21 13:31:09 +0800

    Site updated: 2025-04-21 13:31:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/midjourney-dragon-ball-avatar/"];function toRandomPost(){
+var posts=["undefined/iphone17-pro-camera-upgrade/","undefined/midjourney-dragon-ball-avatar/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
