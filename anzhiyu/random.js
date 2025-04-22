8771c75caf0c83a50fb5561bc5f55af806c20fe5
Weilakuba <oliver.wcc@gmail.com>
2025-04-22 19:51:36 +0800

    Site updated: 2025-04-22 19:51:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/midjourney-gundam-coloring-book/","undefined/create-your-own-comic-style-avatar/","undefined/iphone17-pro-camera-upgrade/","undefined/midjourney-ukiyo-e-anime/","undefined/midjourney-dragon-ball-avatar/","undefined/tesla-updates-mission-statement/"];function toRandomPost(){
+var posts=["undefined/create-your-own-comic-style-avatar/","undefined/iphone17-pro-camera-upgrade/","undefined/midjourney-gundam-coloring-book/","undefined/midjourney-dragon-ball-avatar/","undefined/tesla-updates-mission-statement/","undefined/midjourney-ukiyo-e-anime/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
